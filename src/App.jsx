3109b75376f2654f21a8f132b5dcd9307151e533
gowtham1194 <gowtham.s.ece.2022@snsce.ac.in>
2025-05-30 21:59:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Greetings from './components/Greetings'
+function App() {
+  return (
+    <div>
+      <Greetings/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Greetings() {
+  let name = "Gautham";
+
+  return (
+    <header>
+      <h2> "Hello, {name}!"</h2>
+    </header>
+  );
+}
+
+export default Greetings;
